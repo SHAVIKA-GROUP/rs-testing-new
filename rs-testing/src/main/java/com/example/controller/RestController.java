5ac28561922912904e28acc4a1f6e21c587f1be4
MaheshaMB <mbmahesha47@gmail.com>
2017-02-27 23:01:34 +0530

    second commit

--- rs-testing/src/main/java/com/example/controller/RestController.java
+++ rs-testing/src/main/java/com/example/controller/RestController.java
@@ -1,9 +1,14 @@
 package com.example.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping(value = "/rService")
@@ -15,4 +20,9 @@ public class RestController {
 		return new ResponseEntity<String>(returnsyncOrders, HttpStatus.OK);
 	}
 
+	@RequestMapping(value = "/post", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> postsyncData(@RequestParam String name, @RequestParam int age) throws Exception {
+		String returnValue = " \"name\" : \""+name+"\",  \"age\" : "+age+" ";
+		return new ResponseEntity<String>(returnValue, HttpStatus.OK);
+	}
 }
--- rs-testing/src/main/webapp/index.jsp
+++ rs-testing/src/main/webapp/index.jsp
@@ -1,6 +1,11 @@
 <html>
 <body>
-<h2>Hello World!</h2>
-<h3>JAX-RS testing page</h3> 
+	<h2>Hello World!</h2>
+	<h3>JAX-RS testing page</h3>
+	<form method="post" action="/rs-testing/rService/post">
+		<label>Name</label><input name="name" id="name" value="" /> <br /> <label>age</label><input
+			name="age" id="age" value="" /> <br />
+		<button type="submit" value="submit">push</button>
+	</form>
 </body>
 </html>
